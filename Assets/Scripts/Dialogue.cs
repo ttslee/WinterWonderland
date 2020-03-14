e3da14bf7c303c669f30776610833dbcd8a3a24e
Johnny Ngo <jdngo2@uci.edu>
2020-03-14 05:31:30 -0700

    colliders

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -6,16 +6,18 @@ using UnityEngine.UI;
 public class Dialogue : MonoBehaviour {
 
     public Text textBox;
+    private string defaultText = "You are not near any objects to use \"E\" on";
+    private string text;
 
 	// Use this for initialization
 	void Start () {
-
+        text = defaultText;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.E))
-            Say("Hello");
+        if (Input.GetKeyDown(KeyCode.E) && text != defaultText)
+            Say(text);
     }
 
     private void Say(string sentence)
@@ -23,6 +25,39 @@ public class Dialogue : MonoBehaviour {
         textBox.text = sentence;
     }
 
-    
+    private void OnTriggerEnter(Collider collision)
+    {
+        switch (collision.tag)
+        {
+            case "Campfire":
+                break;
+            case "Igloo":
+                break;
+            case "RadioTower":
+                break;
+            case "Toad":
+                break;
+            case "Tent":
+                break;
+            case "Stump":
+                break;
+            case "Tree":
+                break;
+            case "Rock":
+                break;
+            case "Sign":
+                break;
+            case "Snowman":
+                break;
+            default:
+                text = defaultText;
+                break;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        text = defaultText;
+    }
 
 }
