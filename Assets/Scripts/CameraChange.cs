1422e38df474f863f2584bae6e3cf6066e525fa4
Johnny Ngo <jdngo2@uci.edu>
2020-03-16 12:04:01 -0700

    camera change y not

---
+++ Assets/Scripts/CameraChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour {
+
+    public Camera tpCam;
+    public Camera fpCam;
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            ShowFirstPersonView();
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ShowThirdPersonView();
+        }
+    }
+
+    public void ShowFirstPersonView()
+    {
+        fpCam.enabled = true;
+        tpCam.enabled = false;
+    }
+
+    public void ShowThirdPersonView()
+    {
+        fpCam.enabled = false;
+        tpCam.enabled = true;
+    }
+}
